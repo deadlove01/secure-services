74de2f17b87214953043bc2c22560e4233ebf7b1
Tuan LM <tuanle2@carp.vn>
2022-09-07 13:45:56 +0700

    save code

---
+++ SecureServices/Movie.API/Configuration/Config.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movie.API.Data;
+
+namespace Movie.API.Configuration;
+
+public static class Config
+{
+    public static void SeedDatabase(this IServiceProvider serviceProvider)
+    {
+        using (var scope = serviceProvider.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+            var moviesContext = services.GetRequiredService<MoviesContext>();
+            MoviesContextSeed.SeedAsync(moviesContext);
+        }
+    }
+
+    public static void ConfigureServices(this IServiceCollection services)
+    {
+        services.AddDbContext<MoviesContext>(opt => opt.UseInMemoryDatabase("Movies"));
+    }
+}
---
+++ SecureServices/Movie.API/Data/MoviesContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Movie.API.Data;
+
+public class MoviesContext : DbContext
+{
+    public MoviesContext (DbContextOptions<MoviesContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Models.Movie> Movie { get; set; }
+}
